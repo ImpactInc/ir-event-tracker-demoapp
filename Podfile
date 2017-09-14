c1d021c47ea1b4dda83518f9e6f4d3e2324971b3
Junsheng Guo <junsheng@impactradius.com>
2017-09-13 17:07:09 -0700

    first commit, a stand alone repo for the event tracker demo app

---
+++ Podfile
@@ -0,0 +1,3 @@
+target 'event-tracker-demoapp' do
+  pod 'ir-event-tracker', '0.0.1'
+end
---
+++ event-tracker-demoapp/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  event-tracker-demoapp
+//
+//  Created by Junsheng.
+//  Copyright © 2017 Impact Radius Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "irAppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([irAppDelegate class]));
+    }
+}
